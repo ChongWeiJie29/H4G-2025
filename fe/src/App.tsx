062002dfd657c299e2f7a443eaf9cd5577bb16a2
NicholasTanYY <nicholastanyunyu@gmail.com>
2025-01-17 20:21:21 +0800

    Add icon and label to tab header

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -12,24 +12,42 @@ import AdminPage from "./pages/AdminPage";
 import TransactionHistory from "./pages/TransactionHistory";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import { AuthProvider } from "./components/General/AuthContext";
+import { useEffect } from "react";
 
-const App = () => (
-  <AuthProvider>
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/createPassword" element={<UniqueInviteLink />} />
-          <Route path="/forgetPassword" element={<ForgetPassword />} />
-          <Route path="/dashboard" element={<UserDashboard />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/transaction-history" element={<TransactionHistory />} />
-          <Route path="/voucher-request" element={<RequestVoucher />} />
-          <Route path="/shop" element={<UserMinimart />} />
-          <Route path="/cart" element={<UserShoppingCart />} />
-                    {/*<Route path="/admin" element={<AdminPage />} />
-                */}
-          <Route
+const setFavicon = (url: string) => {
+  const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = url;
+  document.head.appendChild(link);
+};
+
+const App: React.FC = () => {
+  useEffect(() => {
+    document.title = "Muhammadiyah Welfare Home";
+    setFavicon('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUovaIrSP9QWrJWNI_ZmR6A7BazFYG6bAP7w&s');
+  }, []);
+
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/createPassword" element={<UniqueInviteLink />} />
+            <Route path="/forgetPassword" element={<ForgetPassword />} />
+            <Route path="/dashboard" element={<UserDashboard />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route
+              path="/transaction-history"
+              element={<TransactionHistory />}
+            />
+            <Route path="/voucher-request" element={<RequestVoucher />} />
+            <Route path="/shop" element={<UserMinimart />} />
+            <Route path="/cart" element={<UserShoppingCart />} />
+            {/*<Route path="/admin" element={<AdminPage />} />
+             */}
+            <Route
               path="/admin"
               element={
                 <ProtectedRoute>
@@ -37,7 +55,7 @@ const App = () => (
                 </ProtectedRoute>
               }
             />
-          {/* Save this for the last step
+            {/* Save this for the last step
           <Route
               path="/admin"
               element={
@@ -46,10 +64,11 @@ const App = () => (
                 </ProtectedRoute>
               }
             /> */}
-        </Routes>
-      </Router>
-    </CartProvider>
-  </AuthProvider>
-);
+          </Routes>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
+  );
+};
 
 export default App;
